8d13806441e8720ca3c2fc37d0a86e6f1f2ee538
LinKinoMalandrin <armand.royneur@gmail.com>
2018-05-25 19:10:03 +0200

    trouver quarterbacks

---
+++ armand.py
@@ -0,0 +1,11 @@
+from constants import *
+
+def findQuarterbacksWithNuts(data):
+	lst = []
+	for i in range(len(data[D_TEAMS])):
+		if i == data[D_NUM_T]:
+			continue
+		quarter = data[D_TEAMS][i][T_PLAYERS][0]
+		if quarter[2] == M_NUT:
+			lst.append((quarter[0], quarter[1]))
+	return lst
